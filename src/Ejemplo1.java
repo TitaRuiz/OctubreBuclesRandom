624b0c08a8560174d5f5925475e11e08a6365a95
TitaRuiz <ggp2002es@gmail.com>
2023-10-19 15:52:36 +0200

    Initial commit

--- src/Ejemplo1.java
+++ src/Ejemplo1.java
@@ -1,7 +1,14 @@
-public class Main {
+import java.util.Scanner;
+
+public class Ejemplo1 {
     public static void main(String[] args) {
-        //Mostrar la tabla de multiplicar del 5
-        int tabla = 5;
+        //Mostrar la tabla de multiplicar del numero leido por consola
+        int tabla;
+        Scanner teclado = new Scanner(System.in);
+
+        //Solicitarlo por la consola
+        System.out.println("Escriba el número de la tabla a mostrar");
+        tabla = teclado.nextInt();
 
         for(int i = 1;i<=10;i++){
             System.out.println(tabla + " * "+ i + " = "+ tabla*i );
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Mostrar la tabla de multiplicar del 5
+        int tabla = 5;
+
+        for(int i = 1;i<=10;i++){
+            System.out.println(tabla + " * "+ i + " = "+ tabla*i );
+        }
     }
 }
